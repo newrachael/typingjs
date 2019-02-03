1ffabf5a2ac55a6df695ce9a4ea31fe660c01be0
newrachael <phillar85@gmail.com>
2019-02-03 14:08:54 +0900

    For korean input
    - kr split function has been added

--- src/typing.js
+++ src/typing.js
@@ -1,5 +1,36 @@
+const krsplit = function (txt) {
+  const _chosung = [ 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' ];
+  const _jungsung = [ 'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ' ];
+  const _jongsung = [ '', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' ];
+  let length = txt.length;
+  var result = [];
+  for ( var i = 0 ; i < length ; i++ ) {
+    var charCode = txt.charCodeAt(i), chosung, jungsung, jongsung;
+
+    // For korean
+    if (charCode < 0xAC00 || charCode > 0xD7A3 ) {
+      result.push(
+        {isKorean:false , value:charCode, order: 0}
+      );
+    } else {
+      charCode = charCode - 0xAC00;
+      jongsung = charCode % 28; // 종성
+      jungsung = ((charCode - jongsung) / 28 ) % 21; // 중성
+      chosung = (((charCode - jongsung) / 28 ) - jungsung ) / 21; // 초성
+      result.push(
+        {isKorean: true, value: _chosung[chosung], order: 1, index: chosung},
+        {isKorean: true, value: _jungsung[jungsung], order: 2, index: jungsung}
+      );
+      if (_jongsung[jongsung] !== '') {
+        result.push({isKorean: true, value: _jongsung[jongsung], order: 3, index: jongsung});
+      }
+    }
+  }
+  return result;
+}
+
 export default function Typing(element, txt) {
-  let index = 0, container;
+  let index = 0, container, charArray, printedString = '';
   function _CSSStyle() {
     var style = '';
     style += '@keyframes blink{';
@@ -25,16 +56,32 @@ export default function Typing(element, txt) {
 
     container = document.createElement('span');
     element.insertAdjacentElement('afterend', container);
-
+  }
+  function setCharAt(str,index,chr) {
+    if(index > str.length-1) { return str };
+    return str.substr(0,index) + chr + str.substr(index+1);
   }
   function type() {
-    if (index < txt.length) {
-      container.textContent += txt[index];
+    var charCode;
+    if (index < charArray.length) {
+      if ( !charArray[index].isKorean || charArray[index].order == 1) {
+        printedString += String.fromCharCode(charArray[index].value);
+      } else {
+        if ( charArray[index].order == 2 ) {
+          charCode = 44032 + ( charArray[index-1].index  * 588 ) + (charArray[index].index*28);
+        } else if ( charArray[index].order == 3 ){
+          charCode = 44032 + ( charArray[index-2].index  * 588 ) + (charArray[index-1].index*28) + charArray[index].index;
+        }
+        printedString = setCharAt(printedString, printedString.length - 1, String.fromCharCode(charCode));
+      }
+      container.textContent = printedString;
       index++;
       setTimeout(type, 250);
     }
   }
   function start() {
+    charArray = krsplit(txt);
+
     init();
     type();
   }
